7a11a5aa46407887b54169a4fdfbb84fa00542f2
Lauchmelder23 <robert.trololo@gmail.com>
2021-09-24 19:22:39 +0200

    implemented glm

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,5 +1,21 @@
 #include <Quark.hpp>
 
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+glm::mat4 camera(float Translate, const glm::vec2& Rotate)
+{
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+	View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+	View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+
+	return Projection * View * Model;
+}
+
 class ExampleLayer : public Quark::Layer
 {
 public:
