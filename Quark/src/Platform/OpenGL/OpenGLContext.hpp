60ed036f851fdee0cf35ef8162dae5cc8d7f210d
Lauchmelder23 <robert.trololo@gmail.com>
2021-09-25 17:25:48 +0200

    added comments

--- Quark/src/Platform/OpenGL/OpenGLContext.hpp
+++ Quark/src/Platform/OpenGL/OpenGLContext.hpp
@@ -8,12 +8,27 @@ namespace Quark
 {
 	namespace Photon
 	{
+		/**
+		 * @brief Abstraction of an OpenGL rendering context
+		 */
 		class OpenGLContext : public Context
 		{
 		public:
+			/**
+			 * @brief        Creates a new context but doesn't initialize it.
+			 * 
+			 * @param handle A pointer to the window API, in this case GLFW
+			 */
 			OpenGLContext(GLFWwindow* handle);
 
+			/**
+			 * @brief Initialize a new OpenGL context and make it current. Set up GLAD.
+			 */
 			void Init() override;
+
+			/**
+			 * @brief Swap the contexts frame buffers
+			 */
 			void SwapBuffers() override;
 
 		private:
--- Quark/src/Quark/Photon/Context.hpp
+++ Quark/src/Quark/Photon/Context.hpp
@@ -1,13 +1,32 @@
 #pragma once
 
+#include "Quark/Core.hpp"
+
 namespace Quark 
 {
 	namespace Photon
 	{
-		class Context
+		/**
+		 * @brief A universal rendering context abstracting the underlying API
+		 */
+		class QUARK_API Context
 		{
 		public:
+			/**
+			 * @brief Initializes a new rendering context
+			 * 
+			 * This function gets implemented in a derived class, and is then
+			 * responsible for setting up that classes rendering API's context
+			 */
 			virtual void Init() = 0;
+
+			/**
+			 * @brief Displays the frame buffer
+			 * 
+			 * This function gets implemented in a derived class, and is then
+			 * responsible for performing everything that is needed to display
+			 * the current frame buffer on the screen.
+			 */
 			virtual void SwapBuffers() = 0;
 		};
 	}
